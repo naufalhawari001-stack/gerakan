9372a2dd26a7cd817e2aee7e35f31e77ba200391
Azhar Hanin <aris.suharyanto79@gmail.com>
2026-04-28 10:41:47 +0700

    upload maning

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,15 +1,21 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-// Import semua skema yang telah dibuat
+// Import semua skema yang ada di folder schemas
 import news from './schemas/news'
 import category from './schemas/category'
 import author from './schemas/author'
+import agenda from './schemas/agenda'
+import quote from './schemas/quote'
+import settings from './schemas/settings'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    // Daftarkan skema di sini agar muncul di dashboard Sanity Studio
+    // Daftarkan semua di sini agar menu Sidebar Sanity Studio lengkap
     news,
     category,
-    author
+    author,
+    agenda,
+    quote,
+    settings
   ],
 }
